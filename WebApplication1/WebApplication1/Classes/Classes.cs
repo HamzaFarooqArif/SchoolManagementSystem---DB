8d0d5d01ea6916573ef5aaad87587925e569e414
Ayesha <ayeshaatif@gmail.com>
2019-04-20 03:10:58 -0700

    Views of Employee student and courses ..

--- WebApplication1/WebApplication1/Classes/Classes.cs
+++ WebApplication1/WebApplication1/Classes/Classes.cs
@@ -2,18 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.ModelBinding;
 using WebApplication1.Models;
 
 namespace WebApplication1.Classes
 {
     public class connection
     {
-        public static DB11V2Entities db = null;
-        public static DB11V2Entities getDB()
+        public static DB11V2Entities1 db = null;
+        public static DB11V2Entities1 getDB()
         {
             if(db == null)
             {
-                DB11V2Entities db = new DB11V2Entities();
+                DB11V2Entities1 db = new DB11V2Entities1();
             }
             return db;
         }
@@ -25,7 +26,7 @@ namespace WebApplication1.Classes
         {
             Batch modelA = new Batch();
             modelA.Session = model.BatchName;
-            DB11V2Entities db = new DB11V2Entities();
+            DB11V2Entities1 db = new DB11V2Entities1();
             if(!db.Batches.Any(b=>b.Session.Equals(modelA.Session)))
             {
                 db.Batches.Add(modelA);
@@ -42,6 +43,55 @@ namespace WebApplication1.Classes
                     db.Semesters.Add(sm);
                     db.SaveChanges();
                 }
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public class EmployeeAction
+    {
+        public static bool Create(EmployeeViewModels employee)
+        {
+            Employee e = new Employee();
+            Person p = new Person();
+            DB11V2Entities1 db = new DB11V2Entities1();
+            p.Name = employee.Name;
+            p.FatherName = employee.FatherName;
+            p.CNIC = employee.CNIC;
+            p.Contact = employee.Contact;
+            p.Address = employee.Address;
+            e.Salary = employee.Salary;
+            e.Designation = employee.Designation;
+            if(!db.People.Any(b=>b.CNIC.Equals(p.CNIC)))
+            {
+                db.Employees.Add(e);
+                db.People.Add(p);
+                db.SaveChanges();
+                return true;
+            }
+            return false;
+            }
+        }
+
+    public class StudentAction
+    {
+        //public static bool Create (StudentViewModels student)
+        //{
+
+        //}
+    }
+    public class CourseAction
+    {
+        public static bool Create (CoursesViewModels course)
+        {
+            DB11V2Entities1 db = new DB11V2Entities1();
+            Course c = new Course();
+            c.Name = course.Name;
+            if(!db.Courses.Any(b=>b.Name.Equals(c.Name)))
+            {
+                db.Courses.Add(c);
+                db.SaveChanges();
                 return true;
             }
             return false;
--- WebApplication1/WebApplication1/Model1.Designer.cs
+++ WebApplication1/WebApplication1/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Acer\Desktop\Attempts\pull\WebApplication1\WebApplication1\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\USER\Desktop\dbpro_122_152-master\dbpro_122_152-master\WebApplication1\WebApplication1\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
